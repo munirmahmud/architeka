bd2671ba741c585ce19ee9cec2edcb899bd93cba
Munir Mahmud <munirmahmud6@gmail.com>
2020-12-15 18:46:44 +0600

    Scroll behaviour added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,4 +4,15 @@ const hamburgerMenu = document.querySelector('.hamburger-menu');
 hamburgerMenu.addEventListener('click', () => {
     const container = document.querySelector('.container');
     container.classList.toggle('active');
-})
+});
+
+// Add scroll behaviour 
+const scrollTop = document.querySelector('.scroll-btn');
+scrollTop.addEventListener('click', () => {
+    let htmlEl = document.querySelector('html');
+    htmlEl.style.scrollBehavior = 'smooth';
+
+    setTimeout(() => {
+        htmlEl.style.scrollBehavior = 'unset';
+    }, 1000);
+});
